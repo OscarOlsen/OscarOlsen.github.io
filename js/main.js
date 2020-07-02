58bc5143b0b8a76e02e834b7ceace585de91d990
Oscar Lira <TulipOlsen.OneOne@gmail.com>
2020-07-01 20:57:25 -0500

    trabaje en navegador, informacion personal

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let botonNavMovilMostrar = document.getElementById('span-nav-mov-mostrar');
+let botonNavMovilOcultar = document.getElementById('span-nav-mov-ocultar');
+
+botonNavMovilMostrar.addEventListener('click', desplegarNavegadorMovil);
+botonNavMovilOcultar.addEventListener('click', ocultarNavegadorMovil);
+
+function desplegarNavegadorMovil() {
+    let nav = document.getElementById('nav-movil');
+    nav.classList.add('nav-movil-mostrar');
+}
+function ocultarNavegadorMovil() {
+    let nav = document.getElementById('nav-movil');
+    nav.classList.remove('nav-movil-mostrar');
+}
