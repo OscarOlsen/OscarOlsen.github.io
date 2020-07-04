8db765278762cb20012e5f72d741d51a3af9f85c
Oscar Lira <TulipOlsen.OneOne@gmail.com>
2020-07-04 12:36:32 -0500

    mejora de diseno

--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-let elementoScrollMovil = document.getElementsByClassName('span-nav-movil');
-let elementoScrollEscritorio = document.getElementsByClassName('span-nav-escritorio');
-console.log(elementoScrollEscritorio);
-
-for (let i = 0; i < elementoScrollMovil.length; i++) {
-    elementoScrollMovil[i].addEventListener('click', function () {
-        desplazarScroll(i);   
-    });
-    elementoScrollEscritorio[i].addEventListener('click', function () {
-        desplazarScroll(i);   
-    });
-}
-function desplazarScroll(i) {
-    let elementoScroll2 = document.getElementsByClassName('article-scroll');
-    let top = elementoScroll2[i].getBoundingClientRect().top
-    window.scroll({
-        top: top,
-        left: 0,
-        behavior: 'smooth'
-    });
-}
-
-
-
-
-
 let botonNavMovilMostrar = document.getElementById('span-nav-mov-mostrar');
 let botonNavMovilOcultar = document.getElementById('span-nav-mov-ocultar');
 
